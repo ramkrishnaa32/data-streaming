8d7d7d4cdc18cec9da0e0539a18447a7bd9b4bd3
K Ramkrishna Achary <35174887+ramkrishnaa32@users.noreply.github.com>
2025-04-24 14:55:07 +0530

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,34 +1,51 @@
 import socket
-from confluent_kafka import Producer
+from confluent_kafka import Consumer
 from dotenv import load_dotenv
 import os
+import ast
 
-# Loading creds
+# Load environment variables
 load_dotenv()
 username = os.getenv('api_key')
 password = os.getenv('api_secret')
 topic = os.getenv('topic')
+bootstrap_servers = os.getenv('bootstrap_servers')
 
-# Kafka connection
-conf = { 'bootstrap.servers': 'pkc-p11xm.us-east-1.aws.confluent.cloud:9092' ,
-         'security.protocol': 'SASL_SSL',
-         'sasl.mechanisms': 'PLAIN',
-         'sasl.username': username,
-         'sasl.password': password,
-         'client.id': socket.gethostname(),
+# Kafka configuration
+conf = {
+    'bootstrap.servers': bootstrap_servers,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanisms': 'PLAIN',
+    'sasl.username': username,
+    'sasl.password': password,
+    'client.id': socket.gethostname(),
+    'group.id': 'consumer-group-1',
+    'auto.offset.reset': 'earliest'
 }
 
-producer = Producer(conf)
+# Create Consumer instance
+consumer = Consumer(conf)
+consumer.subscribe([topic])
+print(f'Subscribed to topic: {topic}')
 
-customer_id = '256'
-customer_details = '{"order_id":2,"customer_id":256,"customer_fname":"David","customer_lname":"Rodriguez","city":"Chicago","state":"IL","pincode":60625,"line_items":[{"order_item_id":2,"order_item_product_id":1073,"order_item_quantity":1,"order_item_product_price":199.99,"order_item_subtotal":199.99},{"order_item_id":3,"order_item_product_id":502,"order_item_quantity":5,"order_item_product_price":50.0,"order_item_subtotal":250.0},{"order_item_id":4,"order_item_product_id":403,"order_item_quantity":1,"order_item_product_price":129.99,"order_item_subtotal":129.99}]}'
+try:
+    while True:
+        msg = consumer.poll(1.0)  # Timeout in seconds
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
 
-def acked(err, msg):
-    if err is not None:
-        print(f'Failed to deliver message: {msg}, error: {err}')
-    else:
-        print(f'Message produced: {msg}')
+        try:
+            data = ast.literal_eval(msg.value().decode('utf-8'))
+            print(f"Received message: {data}")
+        except Exception as e:
+            print(f"Error decoding message: {e}")
 
-producer.produce(topic=topic, key=customer_id, value=customer_details, callback = acked)
-producer.poll(1)
-producer.flush()
+except KeyboardInterrupt:
+    print("Consumer interrupted. Closing...")
+    consumer.close()
+finally:
+    consumer.close()
+    print("Consumer closed.")
